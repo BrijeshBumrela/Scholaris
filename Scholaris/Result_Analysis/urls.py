d8f62dd43f8b81b28b465bbde3474f86649ba346
BrijeshBumrela <brijeshbumrela@gmail.com>
2018-10-23 21:53:25 +0530

    Added template directory

--- Scholaris/Result_Analysis/urls.py
+++ Scholaris/Result_Analysis/urls.py
@@ -2,6 +2,5 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('Result_Analysis.urls')),
+    path('', views.index, name='index'),
 ]
