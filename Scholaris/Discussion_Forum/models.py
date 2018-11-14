9103aa721b96bf79b91ae4075bfd102139a21826
bhavani shankar <psvnbhavanishankar001@gmail.com>
2018-11-14 16:49:46 +0530

    implemented django signals and slugify - basic create post done

--- Scholaris/Discussion_Forum/models.py
+++ Scholaris/Discussion_Forum/models.py
@@ -3,6 +3,9 @@ from django.db import models
 from django.urls import reverse
 #from Result_Analysis.models import Student,Teacher
 from django.contrib.auth.models import User
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from django.utils.text import slugify
 
 # Create your models here.
 
@@ -26,3 +29,9 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse("forum:forum-question", args=[self.id, self.slug])
+
+@receiver(pre_save, sender=Post)
+def pre_save_slug(sender, **kwargs):
+    slug = slugify(kwargs['instance'].title)
+    kwargs['instance'].slug = slug
+
--- Scholaris/Result_Analysis/models.py
+++ Scholaris/Result_Analysis/models.py
@@ -2,6 +2,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Student(models.Model):
     student = models.OneToOneField(User, on_delete=models.CASCADE)
     dob = models.DateField(blank=True, null=True)
